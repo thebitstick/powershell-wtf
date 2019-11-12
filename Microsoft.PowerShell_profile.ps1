6f7b4090b4230023aaec7862803e03320ef5f133
thebitstick <thebitstick@tfwno.gf>
2019-11-12 13:30:05 -0600

    Comments

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,30 +1,33 @@
-$env:PATH += ":$env:HOME/.local/bin:$env:HOME/.cargo/bin"
+$env:PATH += ":$env:HOME/.local/bin:$env:HOME/.cargo/bin" # Enables Python/Rust user-installed binaries (like sharexin or toot)
 
+# Oh-My-Posh theming
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Impact
 
+# Added for functionality similar to history searching used in FiSH Shell
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
 New-Alias Get-Computer $env:HOME/.local/share/powershell/Scripts/uname.ps1
 New-Alias Get-DiskUsage $env:HOME/.local/share/powershell/Scripts/df.ps1
-New-Alias Edit-Item vim
+New-Alias Edit-Item vim # lol
 New-Alias Connect-Shell ssh
 
+# These aren't implemented by default in Powershell
 function ~ {
 	Set-Location $env:HOME
-}
-
-function .. {
+} function .. {
 	Set-Location ..
 }
 
+# Easy shortcut to the equivalent ~/.bashrc or ~/.zshrc or ~/.config/fish/config.fish
 function Edit-Profile {
 	Edit-Item $PROFILE
 }
 
+# I had made this function in FiSH Shell and it's useful for performing updates
 function Update-Computer {
 	flatpak update
 	sudo dnf upgrade --refresh
@@ -35,9 +38,10 @@ function Edit-PrivilegedItem {
 		[Parameter(Mandatory=$true, Position=1)]
 		[string]$Item
 	)
-	Elevate-Privilege vim $Item
+	Invoke-Sudo vim $Item
 }
 
+# Useful when using a Surface device and you need to ensure usage of the Surface Linux kernel
 function Get-DefaultKernel {
 	class Kernel {
 		[string]$Name
@@ -53,6 +57,7 @@ function Get-DefaultKernel {
 	$kern
 }
 
+# Takes args and calls a new shell with sudo
 function Invoke-Sudo {
 	& /usr/bin/env sudo pwsh -nologo -command "& $args"
 }
