8bea57d8bf3cbfe24fda50846b5f1f922d90306c
thebitstick <thebitstick@tfwno.gf>
2019-11-14 00:46:06 -0600

    Variable for script path & moved 2 functions
    Moved Edit-PrivilegedItem & Invoke-Sudo to separate files

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,7 @@
 $env:PATH += ":$env:HOME/.local/bin:$env:HOME/.cargo/bin" # Enables Python/Rust user-installed binaries (like sharexin or toot)
 
+$SCRIPT_PATH = "$env:HOME/Git/powershell-wtf"
+
 # Oh-My-Posh theming
 Import-Module posh-git
 Import-Module oh-my-posh
@@ -11,9 +13,11 @@ Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
 #New-Alias Send-PSNotification notify-send # installed module, here to just keep track of
-New-Alias Get-ComputerInfo $env:HOME/Git/powershell-wtf/uname.ps1
-New-Alias Get-DiskUsage $env:HOME/Git/powershell-wtf/df.ps1
-New-Alias Get-DefaultKernel $env:HOME/Git/powershell-wtf/grubby.ps1
+New-Alias Get-ComputerInfo $SCRIPT_PATH/uname.ps1
+New-Alias Get-DiskUsage $SCRIPT_PATH/df.ps1
+New-Alias Get-DefaultKernel $SCRIPT_PATH/grubby.ps1
+New-Alias Invoke-Sudo $SCRIPT_PATH/sudo.ps1
+New-Alias Edit-PrivilegedItem $SCRIPT_PATH/sudovi.ps1
 New-Alias Edit-Item vim # lol
 New-Alias Connect-Shell ssh
 New-Alias Open-Item xdg-open
@@ -36,18 +40,4 @@ function Edit-Profile {
 function Update-Computer {
 	flatpak update
 	sudo dnf upgrade --refresh
-}
-
-# sudo vim
-function Edit-PrivilegedItem {
-	param(
-		[Parameter(Mandatory=$true, Position=1)]
-		[string]$Item
-	)
-	Invoke-Sudo vim $Item
-}
-
-# Takes args and calls a new shell with sudo
-function Invoke-Sudo {
-	& /usr/bin/env sudo pwsh-preview -nologo -command "& $args"
 }
