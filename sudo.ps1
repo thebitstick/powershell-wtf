c547ac17a8db854a9aca8588be016f2208e84034
thebitstick <thebitstick@tfwno.gf>
2019-11-14 00:49:45 -0600

    Removed unreachable throw

--- sudo.ps1
+++ sudo.ps1
@@ -3,5 +3,4 @@ param (
 	[string[]]$args = $args
 )
 
-if (-not($args)) { Throw "Invoke-Sudo: You must supply commands"; }
-else { /usr/bin/env sudo pwsh-preview -nologo -command "& $args"; }
+/usr/bin/env sudo pwsh-preview -nologo -command "& $args";
