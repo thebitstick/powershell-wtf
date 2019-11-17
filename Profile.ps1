89d7cefa64659a91ea68c120f66f6e017b036c96
thebitstick <thebitstick@tfwno.gf>
2019-11-17 16:30:34 -0600

    Making it easier to get to this path

--- Profile.ps1
+++ Profile.ps1
@@ -1,5 +1,6 @@
 $env:PATH += ":$env:HOME/.local/bin:$env:HOME/.cargo/bin" # Enables Python/Rust user-installed binaries (like sharexin or toot)
 $WTF_MODULE = "$env:HOME/Git/powershell-wtf/WTF.psm1"
+$WTF_PATH = "$env:HOME/Git/powershell-wtf/"
 
 # Oh-My-Posh theming
 Import-Module $WTF_MODULE
