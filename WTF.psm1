1d6edc39269d9184192a70f72af4eaa53e88fa24
thebitstick <thebitstick@tfwno.gf>
2019-11-18 14:57:40 -0600

    Adding systemctl functions
    Implements #2

--- WTF.psm1
+++ WTF.psm1
@@ -134,7 +134,7 @@ function Get-DefaultKernel {
   #Requires -Version 6.0
 
   [CmdletBinding()]
-  class Kernel{
+  class Kernel {
     [string]$Name
     [string]$Location
   }
@@ -248,7 +248,7 @@ function Get-ComputerInfo {
   $Comp = [ComputerInfo]::new()
   $Os = [OSInfo]::new()
 
-  $OsRelease = ((Get-Content /etc/os-release) -replace '"',''| ConvertFrom-StringData)
+  $OsRelease = ((Get-Content /etc/os-release) -replace '"','' | ConvertFrom-StringData)
 
   $Comp.Kernel = (uname --kernel-name)
   $Comp.Hostname = (uname --nodename)
@@ -315,15 +315,50 @@ function Write-EveryOtherOdd {
 function Get-Service {
 <#
         .SYNOPSIS
-            Alias for systemctl status.
+            Gets the services on the computer.
         .DESCRIPTION
-            Currently is only an alias, but should be feature comparable to Windows equivalent.
+            Retreives services on a computer, including running and stopped services. By default, without parameters, all local services will be returned.
         .INPUTS
-            An (optional) service name.
-	.OUTPUTS
-	    The status of either a single service or every service running.
+            System.String
+        .OUTPUTS
+            Output of systemctl.
         .EXAMPLE
-            Get-Service sshd
+            Get-Service -Name sshd.service
+            Get-Service -DisplayName default-kernel.path
+#>
+
+  #Requires -Version 6.0
+
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $false,ValueFromPipeline = $true)]
+    [string[]]$Name,
+    [Parameter(Mandatory = $false)]
+    [string[]]$DisplayName,
+    [Parameter(Mandatory = $false)]
+    [string[]]$Include,
+    [Parameter(Mandatory = $false)]
+    [string[]]$Exclude
+  )
+
+  $Output = (/usr/bin/env systemctl status $Name $Include $DisplayName)
+
+  $Output
+}
+
+function Start-Service {
+<#
+        .SYNOPSIS
+            Starts one or more stopped services.
+        .DESCRIPTION
+            Sends a start message to systemctl for each of the specified services. If a service is already running, the message is ignored without error. You can specify the services by their service names or display names that represent the services that you want to start.
+        .INPUTS
+            System.String
+        .OUTPUTS
+            If no error is generated, then nothing.
+        .EXAMPLE
+            Start-Service -Name sshd.service
+            Start-Service -Confirm -DisplayName default-kernel.path
 #>
 
   #Requires -Version 6.0
@@ -331,8 +366,136 @@ function Get-Service {
   [CmdletBinding()]
   param(
     [Parameter(Mandatory = $false)]
-    [string[]]$Name
+    [switch]$Confirm,
+    [Parameter(Mandatory = $false,ValueFromPipeline = $true)]
+    [string[]]$Name,
+    [Parameter(Mandatory = $false)]
+    [string[]]$DisplayName,
+    [Parameter(Mandatory = $false)]
+    [string[]]$Include,
+    [Parameter(Mandatory = $false)]
+    [string[]]$Exclude
   )
 
-  /usr/bin/env sudo systemctl status $Name
+  $Question = 'Are you sure you want to perform this action?'
+  $Choices = '&Yes', '&No'
+
+  if ($Confirm) {
+    $Decide = $Host.UI.PromptForChoice('Confirm', $Question, $Choices, 1)
+    if ($Decide  -ne 0) {
+      return
+    }
+  }
+
+  $Output = (/usr/bin/env systemctl start $Name $Include $DisplayName)
+
+  $Output
+}
+
+function Stop-Service {
+<#
+        .SYNOPSIS
+            Stops one or more running services.
+        .DESCRIPTION
+            Sends a stop message to systemctl for each of the specified services. You can specify the services by their service names or display names that represent the services that you want to stop.
+        .INPUTS
+            System.String
+        .OUTPUTS
+            If no error is generated, then nothing.
+        .EXAMPLE
+            Stop-Service -Name sshd.service
+            Stop-Service -Confirm -DisplayName default-kernel.path
+#>
+
+  #Requires -Version 6.0
+
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $false)]
+    [switch]$Confirm,
+    [Parameter(Mandatory = $false)]
+    [switch]$Force,
+    [Parameter(Mandatory = $false,ValueFromPipeline = $true)]
+    [string[]]$Name,
+    [Parameter(Mandatory = $false)]
+    [string[]]$DisplayName,
+    [Parameter(Mandatory = $false)]
+    [string[]]$Include,
+    [Parameter(Mandatory = $false)]
+    [string[]]$Exclude
+  )
+
+  $Question = 'Are you sure you want to perform this action?'
+  $Choices = '&Yes', '&No'
+
+  if ($Confirm) {
+    $Decide = $Host.UI.PromptForChoice('Confirm', $Question, $Choices, 1)
+    if ($Decide  -ne 0) {
+      return
+    }
+  }
+
+  $IsForced = ""
+
+  if ($Force) {
+    $IsForced = "--force"
+  }
+
+  $Output = (/usr/bin/env systemctl stop $IsForced $Name $Include $DisplayName)
+
+  $Output
+}
+
+function Restart-Service {
+<#
+        .SYNOPSIS
+            Stops and then starts one or more services.
+        .DESCRIPTION
+            Sends a stop message to systemctl for each of the specified services, then sends a start message for each of the specified services. You can specify the services by their service names or display names that represent the services that you want to restart.
+        .INPUTS
+            System.String
+        .OUTPUTS
+            If no error is generated, then nothing.
+        .EXAMPLE
+            Restart-Service -Name sshd.service
+            Restart-Service -Confirm -DisplayName default-kernel.path
+#>
+
+  #Requires -Version 6.0
+
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $false)]
+    [switch]$Confirm,
+    [Parameter(Mandatory = $false)]
+    [switch]$Force,
+    [Parameter(Mandatory = $false,ValueFromPipeline = $true)]
+    [string[]]$Name,
+    [Parameter(Mandatory = $false)]
+    [string[]]$DisplayName,
+    [Parameter(Mandatory = $false)]
+    [string[]]$Include,
+    [Parameter(Mandatory = $false)]
+    [string[]]$Exclude
+  )
+
+  $Question = 'Are you sure you want to perform this action?'
+  $Choices = '&Yes', '&No'
+
+  if ($Confirm) {
+    $Decide = $Host.UI.PromptForChoice('Confirm', $Question, $Choices, 1)
+    if ($Decide  -ne 0) {
+      return
+    }
+  }
+
+  $IsForced = ""
+
+  if ($Force) {
+    $IsForced = "--force"
+  }
+
+  $Output = (/usr/bin/env systemctl restart $IsForced $Name $Include $DisplayName)
+
+  $Output
 }
