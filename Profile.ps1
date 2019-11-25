94e0d4c15ceda1b5ee2b721d8cec24e46afcea4e
thebitstick <thebitstick@tfwno.gf>
2019-11-25 00:49:51 -0600

    Implemented #13
    Lamo

--- Profile.ps1
+++ Profile.ps1
@@ -1,10 +1,12 @@
 $env:PATH += ":$env:HOME/.local/bin:$env:HOME/.cargo/bin" # Enables Python/Rust user-installed binaries (like sharexin or toot)
 $SYSTEM_MODULE = "$env:HOME/Git/powershell-systemctl/Systemctl.psm1"
+$MASM_MODULE = "$env:HOME/Git/powershell-wtf/masm.psm1"
 $WTF_MODULE = "$env:HOME/Git/powershell-wtf/WTF.psm1"
 $WTF_PATH = "$env:HOME/Git/powershell-wtf/"
 
 # Oh-My-Posh theming
 Import-Module $SYSTEM_MODULE
+Import-Module $MASM_MODULE
 Import-Module $WTF_MODULE
 Import-Module posh-git
 Import-Module oh-my-posh
@@ -19,10 +21,8 @@ Set-PSReadLineOption -HistorySearchCursorMovesToEnd:$true
 Set-Alias Edit-Item nvim 
 Set-Alias Connect-Shell ssh
 Set-Alias Open-Item xdg-open
-Set-Alias Emulate-Console wineconsole
-Set-Alias Compile-C++ g++
-Set-Alias Compile-Assembly masm
-Set-Alias Compile-Rust rustc
+Set-Alias Build-C++ g++
+Set-Alias Build-Rust rustc
 Set-Alias ls Get-ChildItem # why use GNU ls when Get-ChildItem exists
 # (although I prefer exa on FiSH shell)
 Set-Alias touch New-Item
---
+++ masm.psm1
@@ -0,0 +1,40 @@
+function Build-Assembly {
+<#
+	.SYNOPSIS
+	  Simplifies the linkage and assembly on Microsoft Assembly programs.
+	.DESCRIPTION
+	  Uses the masm assembler through Wine to assemble Assembly programs.
+	.INPUTS
+	  One Microsoft Assembly program file.
+	.OUTPUTS
+	  If no error, then an executable is produced.
+	.EXAMPLE
+	  Build-Assembly -Path AddTwo.asm
+#>
+
+  #Requires -Version 6.0
+
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)]
+    [string]$Path
+  )
+
+  $ErrorActionPreference = 'Stop'
+
+  Set-Variable -Name WINEPREFIX -Value "$env:HOME/.wine"
+
+  New-Variable -Name FileName
+  New-Variable -Name Extension
+
+  $FileName = (Get-Item $Path).BaseName
+  $Extension = (Get-Item $Path).Extension
+
+  if ( $Extension -ne ".asm" ) {
+    Write-Error "Input a valid MASM file."
+  }
+
+  wine ml -nologo -c -coff -Zi "$Path"
+
+  wine link /NOLOGO /SUBSYSTEM:CONSOLE /ENTRY:main /LIBPATH:'C:\Irvine' irvine32.lib kernel32.lib user32.lib ""$FileName".obj"
+}
